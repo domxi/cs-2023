eab15c0e727fd64a9cd31558cee310949f36adc0
cdivinity <140557169+cdivinity@users.noreply.github.com>
2023-07-25 16:38:13 -0500

    Update program.py

--- Courtland/program.py
+++ Courtland/program.py
@@ -1,1 +1,28 @@
+def main():
+    num1 = input("fristnumber: ")
+    operator = input("Operartor: ")
+    num2 = input("Secound Number: ")
+    answer = None
 
+    #addition
+    if operator == "+":
+        answer = int(num1) + int(num2)
+
+    #addition
+    if operator == "-":
+        answer = int(num1) - int(num2)
+
+    #addition
+    if operator == "*":
+        answer = int(num1) * int(num2)
+
+    #addition
+    if operator == "/":
+        answer = int(num1) / int(num2)
+
+
+
+    print(num1 + operator + num2)
+    print(answer)
+
+main()
