2b0cebb3fb902ec6dc6f92c84abb9ff8149e54e6
leonel12323 <140557162+leonel12323@users.noreply.github.com>
2023-07-25 16:40:03 -0500

    Update program.py

--- leo/program.py
+++ leo/program.py
@@ -1,1 +1,24 @@
+def main():
+    num1=input("First #: ")
+    operator=input("Operator: ")
+    num2=input("second #: ")
+    answer=None
 
+
+    #add
+    if operator == "+": 
+        answer = int(num1) + int(num2)  
+    #subtract
+    if operator == "-": 
+        answer = int(num1) - int(num2) 
+    #divide
+    if operator == "/": 
+        answer = int(num1) / int(num2)
+    #multiply
+    if operator == "*": 
+        answer = int(num1) * int(num2)
+    print(num1+ operator + num2)
+    print(answer)
+
+
+main()
